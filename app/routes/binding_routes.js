ed55702e6835a3b6be1fc958527c3283387b231a
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-04-10 10:23:02 +0100

    get binding created

--- app/routes/binding_routes.js
+++ app/routes/binding_routes.js
@@ -4,8 +4,8 @@ module.exports = function(app, db) {
 	
 	app.post('/text',(req,res) => {
 		// res.send('blahblah'); 
-		var accountSid = 'AC4d84cae0c7906cbb2fa772db36c7de9c'; // Your Account SID from www.twilio.com/console
-		var authToken = '66d6c6d74f354e47f352e1555cd902d7';   // Your Auth Token from www.twilio.com/console
+		var accountSid = 'ACe3e780ec614854942e7b3cee487d5282'; // Your Account SID from www.twilio.com/console
+		var authToken = 'c92d577c0d406d917464b0ea74195823';   // Your Auth Token from www.twilio.com/console
 
 		var client = new twilio(accountSid, authToken);
 
@@ -21,6 +21,30 @@ module.exports = function(app, db) {
 		console.log(req.body); 
 		res.send('hello'); 
 
+	}); 
+
+	app.post('/bindings',(req,res) => {
+		console.log(req.body); 
+		let body = req.body; 
+
+		let { identity, binding_type, address } = body; 
+
+		var accountSid = 'ACe3e780ec614854942e7b3cee487d5282';
+		var authToken = 'c92d577c0d406d917464b0ea74195823';
+		var serviceId = 'IS3d6a777f378635bca0070859760a3db8'
+	
+		var client = new twilio(accountSid, authToken);
+		var service = client.notify.services(serviceId);
+
+		service.bindings.create({
+		    identity: identity,
+		    bindingType: binding_type,
+		    address: address
+		  }).then(function(binding) {
+		    console.log(binding);
+		  }).catch(function(error) {
+		    console.log(error);
+		  });
 	}); 
 
 };
