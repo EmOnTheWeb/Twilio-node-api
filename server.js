b2d5fc5484812de45fe84e3120680e3f4921f787
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-04-10 12:01:25 +0100

    accept type json string

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app            = express();
 
 const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require('./app/routes')(app, {});
 app.listen(process.env.PORT || port, () => {
