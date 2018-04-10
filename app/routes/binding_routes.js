f87a22feb2a962beeeb38a14551f9966a745e1cb
emilie.dannenberg@gmail.com <emilie.dannenberg@gmail.com>
2018-04-10 11:41:23 +0100

    handle options request

--- app/routes/binding_routes.js
+++ app/routes/binding_routes.js
@@ -2,8 +2,15 @@ module.exports = function(app, db) {
 
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
+		res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		next();
+		if ('OPTIONS' === req.method) {
+	     	//respond with 200
+	     	res.send(200);
+		} else {
+	    //move on
+	        next();
+	    }
 	});
 
 	const twilio = require('twilio');
